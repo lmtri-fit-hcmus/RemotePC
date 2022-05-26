5d9fda8e89c1634711a8a47a078ecc9d602b69aa
Minh-Tri Le <minhtrikhtnk20@gmail.com>
2022-05-26 23:05:27 +0700

    Add Webcam Capture

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import ReceiveEmail
 import SendEmail
 import ListProcesses
+import WebcamCapture
 
 client_address = "client1.computernetwork@gmail.com"
 client_pass="computernetwork" #unsecure
@@ -11,22 +12,21 @@ def Handling(subject):
     if subject == 'List Processes':
         content = ListProcesses.ListProcesses()
         SendEmail.SendEmail(content,client_address,client_pass,guess_address,subject,'text')
-    elif subject == "demo send picture":
-        content = "test.jpg"
+    elif subject == "Webcam Capture":
+        content = WebcamCapture.CapturePath
         SendEmail.SendEmail(content,client_address,client_pass,guess_address,subject,'image')
 def main():
     while True:
         #Nhận danh sách title của các thư "CHƯA ĐỌC"
-        # list = ReceiveEmail.ReceiveEmail(client_address,client_pass)
-        # if len(list):
-        #     for i in list:
-        #         #Xử lý các title tương ứng
-        #         Handling(i)
-        # else:
-        #     print("Không có thư nào chưa đọc")
-        # break
-        Handling("demo send picture")
+        list = ReceiveEmail.ReceiveEmail(client_address,client_pass)
+        if len(list):
+            for i in list:
+                #Xử lý các title tương ứng
+                Handling(i)
+        else:
+            print("Không có thư nào chưa đọc")
         break
+
 
 #======================================================
 
